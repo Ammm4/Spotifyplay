13bfeca713e9cea5141400b80587786043612a21
Ammm4 <ammm.4amit@gmail.com>
2021-05-21 13:59:47 +0100

    external util file deleted and Error component added

---
+++ src/components/Errors/error.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+class Error extends React.Component {
+  render(){
+    return(
+      <div className='error-container'>Error</div>
+    )
+  }
+}
+
+export default Error;
